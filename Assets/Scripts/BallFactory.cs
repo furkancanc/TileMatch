5dccb0e5d4568d9bbd4c4db723120ab894e5e3be
furkancanc <fcc.ceylan@gmail.com>
2025-03-02 19:30:54 +0300

    Add Special Balls

--- Assets/Scripts/BallFactory.cs
+++ Assets/Scripts/BallFactory.cs
@@ -12,9 +12,21 @@ public class BallFactory : MonoBehaviour
         BallType.Green,
         BallType.Blue
     };
+    private static readonly BallType[] SpecialTypes = new[]
+    {
+        BallType.Bomb,
+        BallType.Reverse,
+        BallType.TimeSlow
+    };
+
+    [Header("Data")]
     [SerializeField] private Sprite redSprite;
     [SerializeField] private Sprite greenSprite;
     [SerializeField] private Sprite blueSprite;
+    [SerializeField] private Sprite bombSprite;
+    [SerializeField] private Sprite reverseSprite;
+    [SerializeField] private Sprite timeSlowSprite;
+
 
     public Ball CreateBallAt(Vector3 point, BallType ballType)
     {
@@ -31,12 +43,22 @@ public class BallFactory : MonoBehaviour
     public Ball CreateRandomBallAt(Vector3 point)
     {
 
-        return CreateBallAt(point, GetRandomBallType());
+        return CreateBallAt(point, GetRandomBallColor());
     }
 
-    private BallType GetRandomBallType()
+    private BallType GetRandomBallColor()
     {
         return Colors[Random.Range(0, Colors.Length)];
+    }
+
+    private BallType GetRandomBallSpecialType()
+    {
+        return SpecialTypes[Random.Range(0, SpecialTypes.Length)];
+    }
+
+    public BallType GetRandomBallType()
+    {
+        return Random.Range(0f, 1f) > 0.2f ? GetRandomBallColor() : GetRandomBallSpecialType();
     }
 
     private Sprite GetSpriteByType(BallType type)
@@ -49,6 +71,12 @@ public class BallFactory : MonoBehaviour
                 return greenSprite;
             case BallType.Blue:
                 return blueSprite;
+            case BallType.Bomb:
+                return bombSprite;
+            case BallType.Reverse:
+                return reverseSprite;
+            case BallType.TimeSlow:
+                return timeSlowSprite;
             default:
                 return blueSprite;
         }
