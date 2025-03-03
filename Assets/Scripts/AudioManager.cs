624798e5d375cc078d54d08df5cde77ae06be2c5
furkancanc <fcc.ceylan@gmail.com>
2025-03-03 20:58:28 +0300

    Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private AudioClip[] sfxList;
+    [SerializeField] private AudioClip[] musicList;
+    [SerializeField] private AudioClip menuMusic;
+
+    [Header("Data")]
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlaySfx(int index)
+    {
+        audioSource.Stop();
+        audioSource.pitch = Random.Range(.9f, 1.1f);
+        audioSource.clip = sfxList[index];
+        audioSource.Play();
+    }
+
+    public void PlayRandomMusic()
+    {
+        audioSource.Stop();
+        audioSource.clip = musicList[Random.Range(0, musicList.Length)];
+        audioSource.Play();
+    }
+
+    public void PlayMenuMusic()
+    {
+        audioSource.Stop();
+        audioSource.clip = menuMusic;
+        audioSource.Play();
+    }
+}
