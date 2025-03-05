e08c5bf69bc979507299d07baf1359ff46f9cde3
furkancanc <fcc.ceylan@gmail.com>
2025-03-05 20:50:49 +0300

    Game Over UI

--- Assets/Scripts/GameUICanvas.cs
+++ Assets/Scripts/GameUICanvas.cs
@@ -1,15 +1,18 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameUICanvas : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI levelTime;
     [SerializeField] private TextMeshProUGUI levelNumber;
     [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject gameUIPanel;
 
     private void Start()
     {
         gameOverPanel.SetActive(false);
+        gameUIPanel.SetActive(true);
     }
 
     public void UpdateLevelTime(float time)
@@ -26,6 +29,17 @@ public class GameUICanvas : MonoBehaviour
 
     public void ShowGameOver()
     {
+        gameUIPanel.SetActive(false);
         gameOverPanel.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
